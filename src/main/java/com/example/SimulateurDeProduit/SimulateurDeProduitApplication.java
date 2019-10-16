e791012bcd935b7f2693c02b969b66bf84f4bb58
Toinou78250 <denisantoi@eisti.eu>
2019-10-16 16:38:11 +0200

    Add files via upload

---
+++ src/main/java/com/example/SimulateurDeProduit/SimulateurDeProduitApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimulateurDeProduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulateurDeProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulateurDeProduitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimulateurDeProduit/SimulateurDeProduitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.SimulateurDeProduit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimulateurDeProduitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
